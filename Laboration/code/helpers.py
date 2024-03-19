1a28b8197f9b690cbde230fc54f68782ec39e89e
Crudeerz <rasmusberghall@gmail.com>
2024-03-19 17:23:27 +0100

    Working on lab

--- Laboration/code/helpers.py
+++ Laboration/code/helpers.py
@@ -10,6 +10,7 @@ def rename_columns(df):
     Parameters: df = pd.Dataframe
 
     """
+
     #Rename columns for plot
     df["cholesterol"] = df["cholesterol"].replace({1:"Normal", 2:"Above normal", 3:"Well above normal"}) # 1 = normal cholesterol, 2 = above normal, 3 = well above normal
     df["gender"] = df["gender"].replace({1:"Women", 2:"Men"}) # 1 = women, 2 = men
@@ -18,3 +19,33 @@ def rename_columns(df):
 
     return df
 
+
+def transform_bmi(BMI):
+    """
+    Returns BMI category represented with numbers: 
+
+    Underweight = 0,
+    Normal weight = 1,
+    Overweight = 2,
+    Obese class 1 = 3,
+    Obese class 2 = 4,
+    Obese class 3 = 5
+    
+    """
+    if BMI <= 18.4:
+        return 0
+    elif 18.5 <= BMI <= 24.9:
+        return 1
+    elif 25.0 <= BMI <= 29.9:
+        return 2
+    elif 30.0 <= BMI <= 34.9:
+        return 3
+    elif 35.0 <= BMI <= 39.9:
+        return 4
+    elif BMI >= 40.0:
+        return 5
+    
+
+def transform_bloodpressure():
+    ...
+    
