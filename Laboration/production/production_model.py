18cce4535bf136a969ba262d5cb20ab039cca6de
Crudeerz <rasmusberghall@gmail.com>
2024-03-23 20:14:58 +0100

    Work on production script

--- Laboration/production/production_model.py
+++ Laboration/production/production_model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import joblib
-import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report, ConfusionMatrixDisplay, confusion_matrix
+
 
 df = pd.read_csv("../data/test_samples.csv")
 X_test, y_test = df.drop(columns="cardio", axis="columns"), df["cardio"]
@@ -9,15 +8,10 @@ X_test, y_test = df.drop(columns="cardio", axis="columns"), df["cardio"]
 
 model = joblib.load(filename="../production//model.pkl")
 
+prediction = model.predict(X_test)
+probability = model.predict_proba(X_test)
 
-y_pred = model.predict(X_test)
-cr = classification_report(y_test, y_pred)
-cm = confusion_matrix(y_test, y_pred)
+df_results = pd.DataFrame(probability, columns=["probability class_0", "probability class_1"], index=None)
+df_results.insert(len(df_results.columns), "prediction", prediction)
 
-# Create and save ConfusionMatrix to folder
-ConfusionMatrixDisplay(cm).plot()
-plt.savefig(f"../production/result_cm.png")
-
-# Write results to result.txt
-with open("../production/result.txt", "a") as f:
-    f.write(f"{cr}\n\n")
+df_results.to_csv("../production/result.csv")
